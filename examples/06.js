d8566e6bde8280df99edc6973b4ee94aedef49f6
Ravinder Negi <r.singh.negi85@gmail.com>
2025-04-06 01:40:30 +0530

    Example 06

---
+++ examples/06.js
@@ -0,0 +1,8 @@
+let nameVar = "Ravi";
+nameVar[0] = "Negi";
+console.log(nameVar[0]);
+
+/* In JavaScript, strings is immutable,which means you cannot change a string directly.
+ * When you use string methods like slice(), toUpperCase(), replace(), etc.
+ * they create a new string rather than modify the original string.
+ */
